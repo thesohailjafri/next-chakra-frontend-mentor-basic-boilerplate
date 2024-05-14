260d3c1395cf56c059ea86c411903d6154bf9256
thesohailjafri <66732877+thesohailjafri@users.noreply.github.com>
2024-05-14 20:25:13 +0530

    chore: Remove unused files and update dependencies

---
+++ src/styles/theme.ts
@@ -0,0 +1,14 @@
+import { extendTheme } from '@chakra-ui/react'
+
+const theme = extendTheme({
+  colors: {
+    primary: {},
+    neutral: {},
+  },
+  fonts: {
+    heading: `sans-serif`,
+    body: `sans-serif`,
+  },
+})
+
+export default theme
